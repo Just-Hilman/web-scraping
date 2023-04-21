6bdc1e2fcab9fbece2168584c277952c7a2bc9cb
Just-Hilman <hilman620@yahoo.com>
2023-04-21 21:56:32 +0700

    Update book web scrap.py

--- 5 Scraping book website/book web scrap.py
+++ 5 Scraping book website/book web scrap.py
@@ -5,3 +5,8 @@ import pandas as pd
 url = "http://books.toscrape.com/catalogue/page-1.html"
 r = requests.get(url)
 print(r)
+
+soup = BeautifulSoup(r.text, "lmxl")
+for i in range(1, 10):
+    np = soup.find_all('a', class_="next").get("href")
+    cnp = "http://books.toscrape.com/catalogue/" + np
