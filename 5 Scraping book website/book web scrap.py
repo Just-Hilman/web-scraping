a197c270d9a084f7fa70fc06b7c013461845fa2a
Just-Hilman <hilman620@yahoo.com>
2023-04-22 20:13:31 +0700

    update

--- 5 Scraping book website/book web scrap.py
+++ 5 Scraping book website/book web scrap.py
@@ -4,9 +4,10 @@ import pandas as pd
 
 url = "http://books.toscrape.com/catalogue/page-1.html"
 r = requests.get(url)
-print(r)
+# print(r)
 
 soup = BeautifulSoup(r.text, "lmxl")
 for i in range(1, 10):
     np = soup.find_all('a', class_="next").get("href")
     cnp = "http://books.toscrape.com/catalogue/" + np
+    print(cnp)
